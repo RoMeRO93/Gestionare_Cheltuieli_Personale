646363acc02f2192737457261f7c4f1507c86be6
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-06-08 12:54:42 +0300

    Add project files.

---
+++ cheltuieli/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace cheltuieli
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            listBox2 = new ListBox();
+            label3 = new Label();
+            listBox3 = new ListBox();
+            label4 = new Label();
+            listBox4 = new ListBox();
+            label5 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(101, 58);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Adauga";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(16, 112);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(160, 184);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(277, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(160, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(176, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Utilitati  /  Intretinere  / Mancare";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(203, 112);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(160, 184);
+            listBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(203, 84);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Urgente";
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(388, 112);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(160, 184);
+            listBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(388, 84);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Curente";
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 15;
+            listBox4.Location = new Point(576, 112);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(160, 184);
+            listBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(576, 84);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Neprevazute";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(288, 58);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 11;
+            button2.Text = "Adauga";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(473, 58);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 12;
+            button3.Text = "Adauga";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(661, 58);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 13;
+            button4.Text = "Adauga";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 600);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label5);
+            Controls.Add(listBox4);
+            Controls.Add(label4);
+            Controls.Add(listBox3);
+            Controls.Add(label3);
+            Controls.Add(listBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private ListBox listBox2;
+        private Label label3;
+        private ListBox listBox3;
+        private Label label4;
+        private ListBox listBox4;
+        private Label label5;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ cheltuieli/Form1.cs
@@ -0,0 +1,62 @@
+﻿namespace cheltuieli
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text; // Accesați textul introdus în textBox1
+
+            listBox1.Items.Add(text); // Adăugați textul în listBox1
+
+            textBox1.Text = "";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text; // Accesați textul introdus în textBox1
+
+            listBox2.Items.Add(text); // Adăugați textul în listBox2
+
+            textBox1.Text = "";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text; // Accesați textul introdus în textBox1
+
+            listBox3.Items.Add(text);
+
+            textBox1.Text = "";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text; // Accesați textul introdus în textBox1
+
+            listBox4.Items.Add(text);
+
+            textBox1.Text = "";
+
+        }
+    }
+}
